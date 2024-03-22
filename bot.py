40c111d8be8b7801a72f4e78924194c05801adc6
igor <svaraigorw@gmail.com>
2024-03-22 16:23:01 +0100

    implementato il bot con tanto di daily task scheduler e rimozione di task (tutte o una)

--- bot.py
+++ bot.py
@@ -16,7 +16,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def send_notification(context: ContextTypes.DEFAULT_TYPE):
-    message = "Reminder!!! Drin Drin"
+    message = f"{context.job.data}"
+    message += "\nReminder!!! Drin Drin"
     await context.bot.send_message(chat_id=context.job.chat_id, text=message)
 
 
@@ -37,15 +38,23 @@ async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await update.effective_message.reply_text("Can't set the timer in the past.")
             return
 
-        job_removed = remove_job_if_exists(str(chat_id), context)
-        context.job_queue.run_once(send_notification, scadenza, name=str(chat_id), chat_id=chat_id)
+        timer_description = None
+        if len(context.args) > 1:
+            timer_description = " ".join(context.args[1:])
 
-        text = "Timer set!"
+        timer_click = datetime.datetime.now() - datetime.timedelta(seconds=scadenza)
+        timer_click_string = timer_click.strftime("%H:%M:%S - %a, %d %b")
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(send_notification, scadenza, name=timer_click_string, chat_id=chat_id,
+                                   data=timer_description)
+
+        text = f"Timer set for {scadenza} seconds. Will ring at: {timer_click_string}"
         if job_removed:
             text += "\nOld one was removed"
         await update.effective_message.reply_text(text)
     except(IndexError, ValueError):
-        await update.effective_message.reply_text("Usage: /set <seconds>")
+        await update.effective_message.reply_text("Usage: /set_t <seconds> timer-description")
 
 
 async def set_daily(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -59,6 +68,10 @@ async def set_daily(update: Update, context: ContextTypes.DEFAULT_TYPE):
         except IndexError:
             second = 0
 
+        timer_description = None
+        if len(context.args) > 1:
+            timer_description = " ".join(context.args[1:])
+
         if not (0 <= hour <= 23) or not (0 <= minute <= 59) or not (0 <= second <= 59):
             await update.effective_message.reply_text("Invalid time format. Please use HH:MM(:SS)? format.")
             return
@@ -67,20 +80,59 @@ async def set_daily(update: Update, context: ContextTypes.DEFAULT_TYPE):
         job_name = datetime_obj.strftime('%H:%M:%S')
 
         job_removed = remove_job_if_exists(job_name, context)
-        context.job_queue.run_daily(send_notification, datetime_obj, name=job_name, chat_id=chat_id)
+        context.job_queue.run_daily(send_notification, datetime_obj, name=job_name, chat_id=chat_id,
+                                    data=timer_description)
 
-        text = f"Timer set at {datetime_obj}!"
+        text = f"Timer set at {datetime_obj} with description: {timer_description}"
         if job_removed:
             text += "\nOld one was removed"
         await update.effective_message.reply_text(text)
     except(IndexError, ValueError):
-        await update.effective_message.reply_text("Usage: /set_daily <HH:MM(:SS)?>")
+        await update.effective_message.reply_text("Usage: /set_d <HH:MM(:SS)?> timer-description")
 
 
 async def unset_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    text = "Timer was successfully removed!" if job_removed else "There is no timer set!"
+    try:
+        timer_to_remove = int(context.args[0])
+
+        all_jobs = context.job_queue.jobs()
+        chat_jobs = [job for job in all_jobs if job.chat_id == chat_id]
+
+        if timer_to_remove > len(chat_jobs) or timer_to_remove < 1:
+            await update.effective_message.reply_text(
+                f"Invalid numer range. Select a valid timer index. [from {1} to {len(chat_jobs)}]")
+            await show_timers(update, context)
+            return
+
+        context.job_queue.jobs()[timer_to_remove-1].schedule_removal()
+        text = f"Timer '{timer_to_remove}' was successfully removed!"
+
+        await update.effective_message.reply_text(text)
+    except (ValueError, IndexError):
+        await update.effective_message.reply_text("Usage: /unset number")
+
+
+async def remove_all_timers(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_message.chat_id
+    chat_jobs = context.job_queue.jobs()
+    text = ""
+    for job in chat_jobs:
+        if job.chat_id == chat_id:
+            text += f"Timer '{job.name}' was successfully removed!\n"
+            job.schedule_removal()
+    if text == "":
+        text = "There is no timer set!"
+    await update.effective_message.reply_text(text)
+
+
+async def show_timers(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    jobs = context.job_queue.jobs()
+    text = "To ineract with the timers use <'HH:MM:SS'>\n\n"
+    if len(jobs) == 0:
+        text = "The list is empty."
+    for i, job in enumerate(jobs):
+        text += f"{i + 1} : {job.name}  -  {job.data}\n"
     await update.effective_message.reply_text(text)
 
 
@@ -90,20 +142,21 @@ def main():
     jq = application.job_queue
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("set", set_timer))
-    application.add_handler(CommandHandler("set_daily", set_daily))
+    application.add_handler(CommandHandler("set_t", set_timer))
+    application.add_handler(CommandHandler("set_d", set_daily))
     application.add_handler(CommandHandler("unset", unset_timer))
+    application.add_handler(CommandHandler("unset_all", remove_all_timers))
+    application.add_handler(CommandHandler("show", show_timers))
 
-    # Avvia il bot
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-
-    # Pianifica l'esecuzione della funzione send_notification() agli orari specificati
-    # tz = pytz.timezone('Europe/Rome')
-    # for time_str in ["10:01", "11:11", "12:21", "13:31", "14:41"]:
-    #     time_obj = datetime.strptime(time_str, "%H:%M").time()
-    #     time_obj = tz.localize(datetime.combine(datetime.today(), time_obj))
-    #     updater.job_queue.run_daily(send_notification, time_obj.time(), days=(0, 1, 2, 3, 4, 5, 6))
 
 
 if __name__ == '__main__':
     main()
+
+# start - Initializes the scheduler
+# set_t - Set a one-time task with description. Usage: /set_t 120 Empty the dishwasher (Insert an amount of seconds from the current moment as the offset)
+# set_d - Set a daily task at the specified time with description. Usage: /set_d 15:45:30 Take out trash (Time can also be without seconds)
+# unset - Deletes the specified timer from the list of active timers. Usage: /unset 5
+# unset_all - Deletes all tasks for the current chat. Usage /unset_all
+# show - Shows all currently active tasks. Usage: /show
